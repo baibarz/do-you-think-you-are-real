3b5f138905e1131b2992e4e31ddd0e05e5f6df70
Mario <m.msteaf@gmail.com>
2021-09-01 19:14:23 +0200

    use flask templet

--- you_are_real/app.py
+++ you_are_real/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    return render_template("index.html", question=123)
 
+@app.route("/update", methods=["POST"])
+def get_response():
+    form_data = request.form
