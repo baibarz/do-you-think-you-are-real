b95246ef5d2483faeb49d8b22d106a66e24a35e6
Mario <m.msteaf@gmail.com>
2021-10-08 23:08:15 +0200

    Opening hours

--- you_are_real/app.py
+++ you_are_real/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def main_page():
-    return render_template("index.html", question=123)
-
+def main_page(): 
+    current_time = datetime.datetime.now().time()  
+    open_time = datetime.time(hour=8)
+    closed_time =datetime.time(hour=16)
+    if current_time >= open_time and current_time <= closed_time:
+        return render_template("index.html", question="do_you_think_you_are_real")
+    else:
+        return render_template("closed.html")
+        
 @app.route("/update", methods=["POST"])
 def get_response():
     form_data = request.form
+
