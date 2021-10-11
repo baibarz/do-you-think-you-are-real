2fb64f14929e644486f20e505a4162850083de1e
Mario <m.msteaf@gmail.com>
2021-10-11 17:09:04 +0200

    Use camelcase in HTML/CSS

--- you_are_real/static/check_open.js
+++ you_are_real/static/check_open.js
@@ -27,7 +27,7 @@ function submitForm() {
     request.open("POST", "/update");
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     request.send(JSON.stringify(data));
-    const answerBox = document.getElementById("answer_input");
+    const answerBox = document.getElementById("AnswerInput");
     answerBox.value = "";
     return false;
 }
