f85b4767ce80c46e6eeade8b7f50875574b6538d
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2022-02-21 22:42:32 +0100

    Five circles

--- you_are_real/static/shit.js
+++ you_are_real/static/shit.js
@@ -1,11 +1,25 @@
 const TICK_INTERVAL = 50;
 const JUMP_DISTANCE = 50;
+const R_CIRCLE = 50;
+const N_TARGETS = 5;
 
 var nextTick;
-var target;
+var drawArea;
+var targets;
 var mouseCoords;
 
 function init() {
+    drawArea = document.getElementById("DrawArea");
+    targets = new Array(N_TARGETS);
+    for (var i = 0; i < N_TARGETS; i++) {
+        const target = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        target.setAttribute("cx", R_CIRCLE + Math.random() * (drawArea.clientWidth - 2 * R_CIRCLE));
+        target.setAttribute("cy", R_CIRCLE + Math.random() * (drawArea.clientHeight - 2 * R_CIRCLE));
+        target.setAttribute("r", R_CIRCLE);
+        drawArea.appendChild(target);
+        targets[i] = target;
+    }
+
     target = document.getElementById("Target");
     nextTick = Number(new Date());
     handleTick();
@@ -29,22 +43,29 @@ function vec_dist(v) {
     return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
 }
 
+function moveBounded(target, pos) {
+    target.setAttribute("cx", Math.max(R_CIRCLE, Math.min(pos.x, drawArea.clientWidth - R_CIRCLE)));
+    target.setAttribute("cy", Math.max(R_CIRCLE, Math.min(pos.y, drawArea.clientHeight - R_CIRCLE)));
+}
+
 function handleTick() {
     if (mouseCoords === undefined) {
         setNextTick();
         return;
     }
-    const p0 = {
-        x: parseFloat(target.getAttribute("cx")),
-        y: parseFloat(target.getAttribute("cy"))
-    }
-    const delta = vec_add(p0, vec_mult(mouseCoords, -1));
-    const dist = vec_dist(delta);
-    if (dist < 150) {
-        const dir = vec_mult(delta, 1 / dist);
-        const pF = vec_add(p0, vec_mult(dir, JUMP_DISTANCE));
-        target.setAttribute("cx", pF.x);
-        target.setAttribute("cy", pF.y);
+    for (var i = 0; i < N_TARGETS; i++) {
+        const target = targets[i];
+        const p0 = {
+            x: parseFloat(target.getAttribute("cx")),
+            y: parseFloat(target.getAttribute("cy"))
+        }
+        const delta = vec_add(p0, vec_mult(mouseCoords, -1));
+        const dist = vec_dist(delta);
+        if (dist < 150) {
+            const dir = vec_mult(delta, 1 / dist);
+            const pF = vec_add(p0, vec_mult(dir, JUMP_DISTANCE));
+            moveBounded(target, pF);
+        }
     }
     setNextTick();
 }
