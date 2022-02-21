cac4264369098c8d5d394c8875d7abcd5bc4b40c
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2022-02-21 21:11:38 +0100

    Vector functions

--- you_are_real/static/shit.js
+++ you_are_real/static/shit.js
@@ -2,18 +2,41 @@ function init() {
 
 }
 
+function vec_add(v1, v2) {
+    return {
+        x: v1.x + v2.x,
+        y: v1.y + v2.y
+    }
+}
+
+function vec_mult(v, a) {
+    return {
+        x: v.x * a,
+        y: v.y * a
+    }
+}
+
+function vec_dist(v) {
+    return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
+}
+
 function handleMouseMove(evt) {
     const target = document.getElementById("Target");
-    const x0 = parseFloat(target.getAttribute("cx"));
-    const dX = x0 - evt.clientX;
-    const y0 = parseFloat(target.getAttribute("cy"));
-    const dY = y0 - evt.clientY;
-    const dist = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
+    const p0 = {
+        x: parseFloat(target.getAttribute("cx")),
+        y: parseFloat(target.getAttribute("cy"))
+    }
+    const mouseCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+    }
+    const delta = vec_add(p0, vec_mult(mouseCoords, -1));
+    const dist = vec_dist(delta);
     if (dist < 150) {
-        const dirX = dX / dist;
-        const dirY = dY / dist;
-        target.setAttribute("cx", x0 + 50 * dirX);
-        target.setAttribute("cy", y0 + 50 * dirY);
+        const dir = vec_mult(delta, 1 / dist);
+        const pF = vec_add(p0, vec_mult(dir, 50));
+        target.setAttribute("cx", pF.x);
+        target.setAttribute("cy", pF.y);
     }
 }
 
