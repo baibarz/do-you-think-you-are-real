273e93817837986fc004343e843848f2fdb5b5b3
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2022-02-21 21:52:38 +0100

    Use polling loop

--- you_are_real/static/shit.js
+++ you_are_real/static/shit.js
@@ -1,5 +1,14 @@
-function init() {
+const TICK_INTERVAL = 50;
+const JUMP_DISTANCE = 50;
 
+var nextTick;
+var target;
+var mouseCoords;
+
+function init() {
+    target = document.getElementById("Target");
+    nextTick = Number(new Date());
+    handleTick();
 }
 
 function vec_add(v1, v2) {
@@ -20,23 +29,36 @@ function vec_dist(v) {
     return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
 }
 
-function handleMouseMove(evt) {
-    const target = document.getElementById("Target");
+function handleTick() {
+    if (mouseCoords === undefined) {
+        setNextTick();
+        return;
+    }
     const p0 = {
         x: parseFloat(target.getAttribute("cx")),
         y: parseFloat(target.getAttribute("cy"))
-    }
-    const mouseCoords = {
-        x: evt.clientX,
-        y: evt.clientY
     }
     const delta = vec_add(p0, vec_mult(mouseCoords, -1));
     const dist = vec_dist(delta);
     if (dist < 150) {
         const dir = vec_mult(delta, 1 / dist);
-        const pF = vec_add(p0, vec_mult(dir, 50));
+        const pF = vec_add(p0, vec_mult(dir, JUMP_DISTANCE));
         target.setAttribute("cx", pF.x);
         target.setAttribute("cy", pF.y);
+    }
+    setNextTick();
+}
+
+function setNextTick() {
+    nextTick += TICK_INTERVAL;
+    setTimeout(handleTick, Math.max(0, nextTick - Number(new Date())));
+}
+
+
+function handleMouseMove(evt) {
+    mouseCoords = {
+        x: evt.clientX,
+        y: evt.clientY
     }
 }
 
