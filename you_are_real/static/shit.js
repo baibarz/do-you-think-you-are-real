9ef1237cec69f5c3dd65d8772f7387ac04c3bcc7
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2022-02-21 23:13:30 +0100

    Add win condition

--- you_are_real/static/shit.js
+++ you_are_real/static/shit.js
@@ -2,6 +2,7 @@ const TICK_INTERVAL = 50;
 const JUMP_DISTANCE = 50;
 const R_CIRCLE = 50;
 const N_TARGETS = 5;
+const EPSILON = 1;
 
 var nextTick;
 var drawArea;
@@ -19,27 +20,41 @@ function init() {
         drawArea.appendChild(target);
         targets[i] = target;
     }
-
-    target = document.getElementById("Target");
     nextTick = Number(new Date());
     handleTick();
 }
 
-function vec_add(v1, v2) {
+function getCoords(target) {
+    return {
+        x: parseFloat(target.getAttribute("cx")),
+        y: parseFloat(target.getAttribute("cy"))
+    };
+}
+
+function vecsAdd(v1, v2) {
     return {
         x: v1.x + v2.x,
         y: v1.y + v2.y
     }
 }
 
-function vec_mult(v, a) {
+function vecMult(v, a) {
     return {
         x: v.x * a,
         y: v.y * a
     }
 }
 
-function vec_dist(v) {
+function vecsEqual(v1, v2) {
+    return vecDist(v1, v2) < EPSILON;
+}
+
+function vecDist(v1, v2) {
+    var delta = vecsAdd(v1, vecMult(v2, -1));
+    return vecLen(delta);
+}
+
+function vecLen(v) {
     return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
 }
 
@@ -49,21 +64,21 @@ function moveBounded(target, pos) {
 }
 
 function handleTick() {
+    if (checkVictory()) {
+        return;
+    }
     if (mouseCoords === undefined) {
         setNextTick();
         return;
     }
     for (var i = 0; i < N_TARGETS; i++) {
         const target = targets[i];
-        const p0 = {
-            x: parseFloat(target.getAttribute("cx")),
-            y: parseFloat(target.getAttribute("cy"))
-        }
-        const delta = vec_add(p0, vec_mult(mouseCoords, -1));
-        const dist = vec_dist(delta);
+        const p0 = getCoords(target);
+        const delta = vecsAdd(p0, vecMult(mouseCoords, -1));
+        const dist = vecLen(delta);
         if (dist < 150) {
-            const dir = vec_mult(delta, 1 / dist);
-            const pF = vec_add(p0, vec_mult(dir, JUMP_DISTANCE));
+            const dir = vecMult(delta, 1 / dist);
+            const pF = vecsAdd(p0, vecMult(dir, JUMP_DISTANCE));
             moveBounded(target, pF);
         }
     }
@@ -75,6 +90,17 @@ function setNextTick() {
     setTimeout(handleTick, Math.max(0, nextTick - Number(new Date())));
 }
 
+function checkVictory() {
+    const coords = getCoords(targets[0]);
+    for (var i = 1; i < N_TARGETS; i++) {
+        if (!vecsEqual(coords, getCoords(targets[i]))) { return false; }
+    }
+    alert("win!");
+    for (var i = 0; i < N_TARGETS; i++) {
+        drawArea.removeChild(targets[i]);
+    }
+    return true;
+}
 
 function handleMouseMove(evt) {
     mouseCoords = {
