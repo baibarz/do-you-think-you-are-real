17d7adff595a4f4f1c2869b134581e416d24ac8d
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2022-02-23 05:17:21 +0100

    Refactored targets into js objects

--- you_are_real/static/shit.js
+++ you_are_real/static/shit.js
@@ -1,38 +1,20 @@
-const TICK_INTERVAL = 50;
 const CURSOR_JUMP_DIST = 3000;
-const REPULSE_JUMP_DIST = 1000;
-const TARGET_SIZE = 120;
-const N_TARGETS = 5;
 const EPSILON = 1;
+const N_TARGETS = 5;
+const REPULSE_JUMP_DIST = 100000;
+const TARGET_SIZE = 120;
+const TICK_INTERVAL = 50;
 
-var nextTick;
+var clientSize;
 var drawArea;
-var targets;
+var nextTick;
+var margins;
 var mouseCoords;
+var targets;
 
-function init() {
-    drawArea = document.getElementById("DrawArea");
-    targets = new Array(N_TARGETS);
-    for (var i = 0; i < N_TARGETS; i++) {
-        const target = document.createElementNS("http://www.w3.org/2000/svg", "image");
-        target.setAttribute("href", "images/poop.svg");
-        target.setAttribute("x", Math.random() * (drawArea.clientWidth - TARGET_SIZE));
-        target.setAttribute("y", Math.random() * (drawArea.clientHeight - TARGET_SIZE));
-        target.setAttribute("width", TARGET_SIZE);
-        target.setAttribute("height", TARGET_SIZE);
-        drawArea.appendChild(target);
-        targets[i] = target;
-    }
-    nextTick = Number(new Date());
-    handleTick();
-}
-
-function getCoords(target) {
-    return {
-        x: parseFloat(target.getAttribute("x")),
-        y: parseFloat(target.getAttribute("y"))
-    };
-}
+/* 
+    Some basic vector functions 
+*/
 
 function vecsAdd(v1, v2) {
     return {
@@ -61,77 +43,130 @@ function vecLen(v) {
     return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
 }
 
-function moveBounded(target, pos) {
-    target.setAttribute("x", Math.max(0, Math.min(pos.x, drawArea.clientWidth - TARGET_SIZE)));
-    target.setAttribute("y", Math.max(0, Math.min(pos.y, drawArea.clientHeight - TARGET_SIZE)));
+// Randomly oriented unit vector
+function vecRand() {
+    const theta = Math.random() * 2 * Math.PI;
+    return {
+        x: Math.cos(theta),
+        y: Math.sin(theta)
+    }
 }
 
+function vecToString(v) {
+    return "x: " + v.x + "\ny:" + v.y;
+}
+
+function vecBound(v) {
+    return {
+        x: Math.max(margins.x, Math.min(v.x, clientSize.x - margins.x)),
+        y: Math.max(margins.y, Math.min(v.y, clientSize.y - margins.y))
+    }
+}
+
+/*
+    Game logic
+*/
+
+function init() {
+    drawArea = document.getElementById("DrawArea");
+    clientSize = {
+        x: drawArea.clientWidth,
+        y: drawArea.clientHeight
+    };
+    margins = {
+        x: TARGET_SIZE / 2,
+        y: TARGET_SIZE / 2
+    };
+    targets = new Array(N_TARGETS);
+    for (var i = 0; i < N_TARGETS; i++) {
+        const pos = {
+            x: TARGET_SIZE / 2 + Math.random() * (drawArea.clientWidth - TARGET_SIZE),
+            y: TARGET_SIZE / 2 + Math.random() * (drawArea.clientHeight - TARGET_SIZE)
+        };
+        const target = document.createElementNS("http://www.w3.org/2000/svg", "image");
+        target.setAttribute("href", "images/poop.svg");
+        target.setAttribute("width", TARGET_SIZE);
+        target.setAttribute("height", TARGET_SIZE);
+        setItemPosition(target, pos);
+        drawArea.appendChild(target);
+        targets[i] = {
+            "ref": target,
+            "pos": pos
+        }
+    }
+    nextTick = Number(new Date());
+    handleTick();
+}
+
+// Main game loop
 function handleTick() {
     if (checkVictory()) {
         return;
     }
-    
+
     // Differences between vector pairs
     const distances = getDistanceMatrix();
 
     // Compute moves for each element
-    for (var i = 0; i < N_TARGETS; i++) {
-        const target = targets[i];
-        const p0 = getCoords(target);
-        const pCenter = {
-            x: p0.x + TARGET_SIZE / 2,
-            y: p0.y + TARGET_SIZE / 2
-        }
-        
-        var pF = p0;
-        
-        // Movement due to cursor
-        if (mouseCoords !== undefined) {
-            const delta = vecsAdd(pCenter, vecMult(mouseCoords, -1));
-            const dist = vecLen(delta);
-            // delta * JUMP_DISTANCE / dist gives a unit vector, so linear dropoff is .. / dist^2
-            const step = vecMult(delta, CURSOR_JUMP_DIST / Math.pow(dist, 2));
-            pF = vecsAdd(pF, step);
-        }
-        
-        // Movement due to mutual repulsion
-        for (var j = 0; j < targets.length; j++) {
-            if (j != i) {
-                const v = distances[i][j];
-                const dst = vecLen(v);
-                if (dst === 0) {
-                    const vRand = {
-                        x: randomRepulse(),
-                        y: randomRepulse()
-                    }
-                    pF = vecsAdd(pF, vRand);
-                }
-                else {
-                    const vu = vecMult(v, 1 / dst);
-                    pF = vecsAdd(pF, vecMult(vu, REPULSE_JUMP_DIST / dst));
-                }
-            }
-        }
-        
-        
-        moveBounded(target, pF);
+    for (var targetIndex = 0; targetIndex < N_TARGETS; targetIndex++) {
+        const target = targets[targetIndex];
+        var pF = applyMouseRepel(target.pos);
+        pF = applyMutualRepel(pF, distances[targetIndex]);
+        pF = vecBound(pF);
+        target.pos = pF;
+        setItemPosition(target.ref, pF);
     }
     setNextTick();
 }
 
-function randomRepulse() {
-    return Math.random() * REPULSE_JUMP_DIST * (Math.round(Math.random() * 2 - 1));
+function setItemPosition(target, pos) {
+    target.setAttribute("x", pos.x - TARGET_SIZE / 2);
+    target.setAttribute("y", pos.y - TARGET_SIZE / 2);
 }
 
-function getDistanceMatrix() {
-    return targets.map(t1 => targets.map(t2 => vecBetweenTargets(t1, t2)));;
-}
-
-function vecBetweenTargets(t1, t2) {
-    if (t1 === t2) {
-        return 0;
+// Movement due to running away from cursor
+function applyMouseRepel(pCenter) {
+    if (mouseCoords === undefined) {
+        return pCenter;
     }
-    return vecsAdd(getCoords(t1), vecMult(getCoords(t2), -1));
+    const delta = vecsAdd(pCenter, vecMult(mouseCoords, -1));
+    const dist = vecLen(delta);
+    // delta * JUMP_DISTANCE / dist gives a unit vector, so linear dropoff is .. / dist^2
+    const step = vecMult(delta, CURSOR_JUMP_DIST / Math.pow(dist, 2));
+    return vecsAdd(pCenter, step);
+}
+
+// Movement due to mutual repulsion
+function applyMutualRepel(pt0, distances) {
+    var pt = pt0;
+    for (var targetIndex = 0; targetIndex < distances.length; targetIndex++) {
+        const diff = distances[targetIndex];
+        const dst = vecLen(diff);
+        if (dst === 0) {
+            // Add some random jitter
+            pt = vecsAdd(pt, vecRand());
+        }
+        else {
+            const vu = vecMult(diff, 1 / dst);
+            pt = vecsAdd(pt, vecMult(vu, REPULSE_JUMP_DIST / Math.pow(dst, 2)));
+        }
+    }
+    return pt;
+}
+
+// Get an array of distances to all other targets from a given target
+function getDistanceMatrix() {
+    return targets.map(t1 =>
+        targets.map(t2 => vecBetweenTargets(t1, t2)).filter(v => v !== undefined)
+    );
+}
+
+// Distance between target points, or undefined if the two targets are the same item
+function vecBetweenTargets(t1, t2) {
+    if (Object.is(t1, t2)) {
+        return undefined;
+    }
+    return vecsAdd(t1.pos, vecMult(t2.pos, -1));
 }
 
 function setNextTick() {
@@ -140,13 +175,12 @@ function setNextTick() {
 }
 
 function checkVictory() {
-    const coords = getCoords(targets[0]);
     for (var i = 1; i < N_TARGETS; i++) {
-        if (!vecsEqual(coords, getCoords(targets[i]))) { return false; }
+        if (!vecsEqual(targets[0].pos, targets[i].pos)) { return false; }
     }
     alert("win!");
     for (var i = 0; i < N_TARGETS; i++) {
-        drawArea.removeChild(targets[i]);
+        drawArea.removeChild(targets[i].ref);
     }
     return true;
 }
@@ -159,5 +193,5 @@ function handleMouseMove(evt) {
 }
 
 function handleClick(evt) {
-    alert("x: " +mouseCoords.x + "\ny: " + mouseCoords.y);
+    alert("x: " + mouseCoords.x + "\ny: " + mouseCoords.y);
 }
