4f2664309f1292c52f882d88b90cbd00842472b0
Anders Lehn Reed <anders.lehn.reed>
2023-07-21 23:28:43 +0200

    no JS globals

--- check_open.js
+++ check_open.js
@@ -1,6 +1,3 @@
-var userId = -1;
-var questionId = 0; // Always starts with "who are you?"
-
 function checkLoad() {
     var now = new Date();
     window.location = (now.getHours() < 8 || now.getHours() > 16 ? "closed.html" : "open.html");
