8e7302d5fc8080b6367e8f2af82065a483523183
bebarz <m.msteaf@gmail.com>
2023-11-02 23:33:50 +0100

    add text speed over time

--- js/shit.js
+++ js/shit.js
@@ -281,12 +281,12 @@ function handleMouseMove(evt) {
 
 
 
-const textArray = []; // Initialize an empty array
+const textArray = [];
 
 // Function to fetch text from a file
 async function fetchTextFromFile() {
     try {
-        const response = await fetch('text/shit.txt'); // Replace 'text.txt' with the actual path to your text file
+        const response = await fetch('text/shit.txt'); 
         const text = await response.text();
         // Split the text into an array using line breaks as separators
         textArray.push(...text.split('\n').map(line => line.trim()));
@@ -294,16 +294,21 @@ async function fetchTextFromFile() {
         console.error('Error fetching text:', error);
     }
 }
-
-// Call the function to fetch text from the file
 fetchTextFromFile().then(() => {
     const textContainer = document.getElementById("text-container");
     let currentIndex = 0;
+    let intervalDuration = 1600; 
+    const intervalFactor = 0.98; 
 
     function changeLanguage() {
         textContainer.textContent = textArray[currentIndex];
         currentIndex = (currentIndex + 1) % textArray.length;
+        intervalDuration *= intervalFactor; 
+
+     
+        setTimeout(changeLanguage, intervalDuration);
     }
 
-    setInterval(changeLanguage, 2000); // Change text every 1.5 seconds
+    setTimeout(changeLanguage, intervalDuration);
 });
+
