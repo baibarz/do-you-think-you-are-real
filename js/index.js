b49296bd09e3dc80447cfb552aa89e0092dfe59c
bebarz <m.msteaf@gmail.com>
2023-11-16 19:51:49 +0100

    nothing

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ fetchTextFromFile().then(() => {
 
 
   function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "260px";
   }
 
   function closeNav() {
